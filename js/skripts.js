0d4ac95bbf768ee984e2ad40c4e4695510c39ffe
MPkekava <54512031+MPkekava@users.noreply.github.com>
2019-09-26 20:18:35 +0300

    Pievienoju fuunkciju

---
+++ js/skripts.js
@@ -0,0 +1,13 @@
+<script>
+    let m1=7;
+    let m2=3;
+    let m3=5;
+    let pazinojums;
+if (m1<m2+m3&&m2<m1+m3&&m3<m1+m2){
+        pazinojums = 'true';
+    console.log(pazinojums);
+}
+else
+pazinojums='false';
+console.log(pazinojums);
+</script>
