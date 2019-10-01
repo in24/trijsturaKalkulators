b31b99af975b1585d14128d42b56373e74771b2d
MPkekava <54512031+MPkekava@users.noreply.github.com>
2019-10-01 20:02:53 +0300

    IrTrijsturis

--- js/skripts.js
+++ js/skripts.js
@@ -2,12 +2,11 @@
     let m1=7;
     let m2=3;
     let m3=5;
-    let pazinojums;
+    var pazinojums;
+    function IrTrijsturis();
 if (m1<m2+m3&&m2<m1+m3&&m3<m1+m2){
-        pazinojums = 'true';
-    console.log(pazinojums);
-}
-else
-pazinojums='false';
-console.log(pazinojums);
+            return "true";
+            } else{ 
+    return "false";
+    console.log(pazinojums)
 </script>
