a723398161020fd36f7e39cd528c20618074aa5e
MPkekava <54512031+MPkekava@users.noreply.github.com>
2019-10-01 21:10:05 +0300

    Maiga_irTrijst

--- js/skripts.js
+++ js/skripts.js
@@ -1,12 +1,11 @@
 <script>
-    let m1=7;
-    let m2=3;
-    let m3=5;
-    var pazinojums;
-    function IrTrijsturis();
-if (m1<m2+m3&&m2<m1+m3&&m3<m1+m2){
-            return "true";
-            } else{ 
-    return "false";
-    console.log(pazinojums)
+    function irTrijsturis(m1,m2,m3){
+        if(m1<m2+m3 && m2<m1+m3 && m3<m1+m2){
+       // r=m1+m2+m3
+       return true;
+        }else{
+            return false;
+        }
+    }
+    console.log(irTrijsturis(2,3,4));
 </script>
